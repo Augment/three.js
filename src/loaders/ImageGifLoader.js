5d0344ff4f63adab3c39fbcee89d0147e8178974
Matthieu Negrier <matthieu.negrier@augment.com>
2018-06-11 17:23:41 +0200

    Create gif extension components

---
+++ src/loaders/ImageGifLoader.js
@@ -0,0 +1,72 @@
+/**
+ * @author augment
+ */
+
+import { AnimatedGifTexture } from '../textures/AnimatedGifTexture.js';
+import { FileLoader } from './FileLoader.js';
+import { DefaultLoadingManager } from './LoadingManager.js';
+import { GifReader } from '../extras/images/GifReader.js';
+
+function ImageGifLoader( manager ) {
+
+	this.manager = ( manager !== undefined ) ? manager : DefaultLoadingManager;
+
+}
+
+Object.assign( ImageGifLoader.prototype, {
+
+	crossOrigin: 'Anonymous',
+
+	load: function ( url, onLoad, onProgress, onError ) {
+
+		var scope = this;
+
+		var texture = new AnimatedGifTexture();
+
+		var loader = new FileLoader( this.manager );
+		loader.setResponseType( 'arraybuffer' );
+
+		loader.load( url, function ( buffer ) {
+
+			var gifComponent = new GifReader( buffer );
+			var frames = gifComponent.decompressFrames( true );
+
+			if ( ! buffer ) return;
+
+			if ( undefined !== frames[ 0 ] ) {
+
+				texture.setFramesData( frames );
+				texture.image.width = frames[ 0 ].dims.width;
+				texture.image.height = frames[ 0 ].dims.height;
+
+			}
+
+			texture.format = THREE.RGBAFormat;
+			texture.type = THREE.UnsignedByteType;
+
+			texture.needsUpdate = true;
+
+			if ( onLoad ) onLoad( texture, buffer );
+
+		}, onProgress, onError );
+
+		return texture;
+
+	},
+
+	setCrossOrigin: function ( value ) {
+
+		this.crossOrigin = value;
+		return this;
+
+	},
+
+	setPath: function ( value ) {
+
+		this.path = value;
+		return this;
+
+	}
+} );
+
+export { ImageGifLoader };
---
+++ src/textures/AnimatedGifTextureAnimation.js
@@ -0,0 +1,163 @@
+/**
+ * @author augment
+ */
+
+function AnimatedGifTextureAnimationTicker() {
+
+	var subscriptions = [];
+
+	function update( time ) {
+
+		subscriptions.forEach( function ( animation ) {
+
+			animation.update( time );
+
+		} );
+
+		if ( 0 !== subscriptions.length ) {
+
+			window.requestAnimationFrame( update );
+
+		}
+
+	}
+
+	this.subscribe = function ( animatedGifTexture ) {
+
+		if ( - 1 === subscriptions.findIndex( function ( elem ) {
+
+			return ( elem === animatedGifTexture );
+
+		} ) ) {
+
+			subscriptions.push( animatedGifTexture );
+
+			if ( 1 === subscriptions.length ) {
+
+				window.requestAnimationFrame( update );
+
+			}
+
+		}
+
+	};
+
+	this.unsubscribe = function ( animatedGifTexture ) {
+
+		var index = subscriptions.findIndex( function ( elem ) {
+
+			return ( elem === animatedGifTexture );
+
+		} );
+
+		if ( - 1 !== index ) {
+
+			subscriptions.splice( index, 0 );
+
+		}
+
+	};
+
+}
+
+function AnimatedGifTextureAnimation( frames, updateCallback ) {
+
+	this.frames = frames;
+
+	this.startTime = null;
+	this.playing = false;
+	this.currentFrameIndex = 0;
+	this.timeline = [];
+	this.duration = 0;
+	this.initialized = false;
+
+	this.onUpdateCallback = updateCallback;
+
+	if ( frames ) {
+
+		this.timeline = frames.map( ( function () {
+
+			var sum = 0.0;
+			return function ( entry ) {
+
+				sum += entry.delay;
+				return sum;
+
+			};
+
+		} )() );
+
+		this.duration = this.timeline[ this.timeline.length - 1 ];
+
+	}
+
+}
+
+Object.assign( AnimatedGifTextureAnimation.prototype, {
+
+	start: function () {
+
+		if ( 1 === this.frames.length )
+			return;
+
+		this.startTime = window.performance.now();
+		this.playing = true;
+		this.currentFrameIndex = 0;
+
+		AnimatedGifTextureAnimation.Ticker.subscribe( this );
+
+	},
+
+	stop: function () {
+
+		this.playing = false;
+		this.initialized = false;
+		AnimatedGifTextureAnimation.Ticker.unsubscribe( this );
+
+	},
+
+	update: function ( time ) {
+
+		if ( 1 === this.frames.length )
+			this.currentFrameIndex = 0;
+
+		if ( this.playing && this.timeline ) {
+
+			if ( ! this.initialized ) {
+
+				this.initialized = true;
+				this.startTime = time;
+
+			}
+
+			var animationTime = ( time - this.startTime ) % this.duration;
+			var t = this.timeline[ this.currentFrameIndex ];
+
+			if ( animationTime < t ) {
+
+				this.currentFrameIndex = 0;
+				t = this.timeline[ this.currentFrameIndex ];
+
+			}
+			while ( animationTime > t ) {
+
+				t = this.timeline[ ++ this.currentFrameIndex ];
+
+			}
+
+		}
+
+		this.onUpdateCallback();
+
+	},
+
+	getFrame: function () {
+
+		return this.frames[ this.currentFrameIndex ];
+
+	}
+} );
+
+AnimatedGifTextureAnimation.Ticker = new AnimatedGifTextureAnimationTicker();
+
+export { AnimatedGifTextureAnimation };
